0c9aa01ba2bbb4b9725ba8168873da0aae2bc1c9
Daniel Bachhuber <daniel@handbuilt.co>
2016-01-25 08:55:03 -0800

    Initial commit

---
+++ bitly-url-generator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Bitly URL Generator
+ * Version: 0.1-alpha
+ * Description: Generates Bitly short URLs for posts
+ * Author: Daniel Bachhuber
+ * Author URI: https://handbuilt.co
+ * Plugin URI: https://handbuilt.co
+ * Text Domain: bitly-url-generator
+ * Domain Path: /languages
+ * @package Bitly URL Generator
+ */
